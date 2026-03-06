4665d7cb7baa8a163c1ed7af44140461355067fa
Will <willkempner@gmail.com>
2026-03-05 23:24:59 -0500

    praying

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  // GitHub Pages project site path (repo: bracket)
+  base: "/bracket/",
   server: {
     port: 4173
   }
